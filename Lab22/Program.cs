331938fa2b7d896f5ea759a894cffce551c972bc
HinaChleck <hinachleck@gmail.com>
2022-01-22 10:04:52 +0500

    ver.0.1

--- Lab22/Program.cs
+++ Lab22/Program.cs
@@ -45,35 +45,27 @@ namespace Lab22
             foreach (var item in array)
             {
                 Console.Write(item + ", ");
-            }   
-            
+            }
+            Console.WriteLine();
         }
         static void Main(string[] args)
         {
             Console.Write("Введите количество чисел в массиве: ");
             uint k = Convert.ToUInt32(Console.ReadLine());
 
-            //int[] array = CreateArray(k);
-            //Task<int[]> task1 = new Task<int[]>(() => CreateArray(k));
-            //Task<int> task2 = task1.ContinueWith(t => SumArray(t.Result));
-            //Task<int> task3 = task1.ContinueWith(t => MaxValue(t.Result));
-            //Task task4 = task1.ContinueWith(t => Print(t.Result));
-            //Task task5 = task2.ContinueWith(t => Console.WriteLine("Сумма чисел в массиве = ", t.Result));
-            //Task task6 = task3.ContinueWith(t => Console.WriteLine("Максимальное число в массиве = ", t.Result));
-            //task1.Start();
-            //task4.Wait();
-            //task5.Wait();
-            //task5.Wait();
-
             int[] array = CreateArray(k);
-            Print(array);
-
-
-            Console.WriteLine("Сумма чисел в массиве = ", SumArray(array));
-            Console.WriteLine("Максимальный элемент в массиве = ", MaxValue(array));
-
-
-            Console.ReadKey(); 
+            Task<int[]> task1 = new Task<int[]>(() => CreateArray(k));
+            Task<int> task2 = task1.ContinueWith(t => SumArray(t.Result));
+            Task<int> task3 = task1.ContinueWith(t => MaxValue(t.Result));
+            Task task4 = task1.ContinueWith(t => Print(t.Result));
+            task1.Start();
+            task4.Wait();
+            if (task4.IsCompleted)
+            {
+                Task task5 = task2.ContinueWith(t => Console.WriteLine("Сумма чисел в массиве = {0}", t.Result));
+                Task task6 = task3.ContinueWith(t => Console.WriteLine("Максимальное число в массиве = {0}", t.Result));
+            }
+           Console.ReadKey(); 
         }
     }
 }
